c63eeceb1717a610618ee4e65b056fd80ff5e955
Silvan Gehrig <silvan.gehrig@bwz-rappi.ch>
2025-07-21 09:09:30 +0200

    Update hero editor sources for ng20.

---
+++ 1-hero-editor/src/app/core/components/heroes.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  imports: [],
+  template: `
+    <p>
+      heroes works!
+    </p>
+  `,
+  styles: ``
+})
+export class Heroes {
+
+}
